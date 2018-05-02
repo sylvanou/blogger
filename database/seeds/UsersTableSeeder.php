ed7ba212170484890c3c5834bbf2a7b31044117f
sylvanou <sylvanoumbac24@gmail.com>
2018-05-02 12:16:17 -0400

    added UsersTableSeeder for dummy data

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Create 10 user rows
+        factory(\App\User::class, 10)->create();
+    }
+}
