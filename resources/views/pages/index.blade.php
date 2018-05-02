18f1131a37dc908333fc66eff35cc6a23b9d32b9
sylvanou <sylvanoumbac24@gmail.com>
2018-05-02 14:36:11 -0400

    edited index view

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,7 +5,11 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Blogger</title>
+        <title>{{config('app.name', 'BLOGGER')}}</title>
+
+    </head>
+    <body>
+      <h1>Welcome to Blogger</h1>
 
     </body>
 </html>
