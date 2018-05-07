17b49fcca9e63c3d56b323f35f8513a16bb66640
sylvanou <sylvanoumbac24@gmail.com>
2018-05-07 11:52:12 -0400

    added flash messaging in the includes folder

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -11,9 +11,6 @@
             {{Form::label('title', 'Body')}}
             {{Form::textarea('body', '', ['id' => 'article-ckeditor', 'class' =>'form-control', 'placeholder' => 'Body Text'])}}
         </div>
-        <div class="form-group">
-            {{Form::file('cover_image')}}
-        </div>
         {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
     {!! Form::close() !!}
 @endsection
